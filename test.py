fca9670abce0898605b0ab6a0f4eaf6365a37c51
Ashish Soni <ashish.soni2091@gmail.com>
2024-04-06 10:15:33 +0000

    test gemini api

--- test.py
+++ test.py
@@ -3,32 +3,21 @@
 import os
 
 # Community modules
+from langchain_google_genai import GoogleGenerativeAI
 # from langchain.chains import LLMChain
 # from langchain_core.output_parsers import StrOutputParser
 # from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 import google.generativeai as genai
 
-
-
-# from openai import OpenAI
-# import requests
-
 # User-Defined modules
 from constants import (LLAMAPARSE_API_KEY,
-                       GEMINIPRO_API_KEY)
+                       LANGCHAIN_API_KEY,
+                       LANGCHAIN_PROJECT,
+                       LANGCHAIN_URL,
+                       GEMINIPRO_API_KEY,
+                       QDRANT_API_KEY,
+                       QDRANT_CLUSTER)
 
-
-# from models import (gpt35_turbo_llm,
-#                     gpt4_llm,
-#                     gpt4_turbo_llm,
-#                     gpt4_vision_llm
-#                     )
-
-# # Set environment variables to enable logging and tracing in LangSmith 
-# os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
-# os.environ["LANGCHAIN_TRACING_V2"] = "true"
-# os.environ["LANGCHAIN_ENDPOINT"] = LANGCHAIN_URL
-# os.environ["LANGCHAIN_PROJECT"] = LANGCHAIN_PROJECT
 
 genai.configure(api_key=GEMINIPRO_API_KEY)
 
@@ -48,12 +37,29 @@ genai.configure(api_key=GEMINIPRO_API_KEY)
 
 # print(llm_chain.invoke(question))
 
-# for m in genai.list_models():
-#     if 'generateContent' in m.supported_generation_methods:
-#         print(m.name)
 
-model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("What is the meaning of life?")
+# model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
-print(response.text)
+# response = model.generate_content("What is the meaning of life?")
+
+# print(response.text)
+
+# Set environment variables to enable logging and tracing in LangSmith 
+os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = LANGCHAIN_URL
+os.environ["LANGCHAIN_PROJECT"] = LANGCHAIN_PROJECT
+
+#### USING LANGCHAIN
+
+llm = GoogleGenerativeAI(model="gemini-1.5-pro-latest", google_api_key=GEMINIPRO_API_KEY)
+
+print(llm.invoke("List 5 reasons to sleep on time everyday. Short and Crisp."))
+
+from qdrant_client import QdrantClient
+
+qdrant_client = QdrantClient(
+    url=QDRANT_CLUSTER, 
+    api_key=QDRANT_API_KEY
+)
